3014a9eae9de1a17a65d82ee8ad2c63af4bebb76
Uddalak Mukhopadhyay <uddalakmukhopadhyay@gmail.com>
2026-05-08 11:24:05 +0530

    fix: resolve vercel cors headers

--- backend/api/index.js
+++ backend/api/index.js
@@ -23,6 +23,11 @@ const connectToDatabase = async () => {
 };
 
 export default async (req, res) => {
+    // Explicitly set CORS headers for Serverless functions
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
+    res.setHeader('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization');
+
     // Handle CORS preflight explicitly
     if (req.method === "OPTIONS") {
         return res.status(200).end();
